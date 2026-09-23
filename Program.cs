74be1189dd286d43182a9e9d58d743f96a06a6e3
Sophiaaaaa382320 <philosophia.alfarabi123@gmail.com>
2026-09-23 13:56:34 +0700

    add "checkpoint" every time step is a multiple of 3

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿int totalsteps = 100;
 for(int a=1; a <= 100; a++)
 {
+    if (a%3==0)
+    {
+        Console.WriteLine("Checkpoint reached!");
+    }
     Console.WriteLine($"Loading.. Step {a} of {totalsteps}");
 }
 Console.WriteLine("\nDungeon is ready. Enter now!");
